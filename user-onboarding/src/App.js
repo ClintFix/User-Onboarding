da02f4610846d817690c1295792494b725ded7e7
ClintFix <clint.fix@gmail.com>
2021-02-24 15:04:17 -0700

    added helper functions in app and form

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import Form from './components/Form'
 import './App.css';
+import Axios from 'axios';
 
 // Initial Conditions for State
 const initialUsers = [
@@ -23,23 +24,74 @@ const initialDisabled = true;
 ///////// APP Component ////////////
 
 function App() {
-// State
-const [user, newUser] = useState(initialUsers) //Array of user objects
+// State //
+const [users, setUsers] = useState(initialUsers) //Array of user objects
 const [formValues, setFormValues] = useState(initialFormValues) //object
 const [formErrors, setFormErrors] = useState(initialFormErrors) //object
 const [disabled, setDisabled] = useState(initialDisabled) //boolean
 
-// Helper functions
+// Helper functions //
 
-// Event handlers
+// Post new user to API
+const postNewUser = newUser => {
+  //axios post
+  //then -> update user in state.
+  // reset form values
+  Axios.post('https://reqres.in/api/users', newUser)
+    .then(res => {
 
+    })
+    .catch(err => {
+
+    })
+  setFormValues(initialFormValues)
+}
+
+// Event handlers //
+
+// onChange 
+// validate with yup
+// .reach (make sure to create and import schema and pass item to validate)
+// validate (pass value to validate)
+// .then (no errors), update formErrors
+// .catch (errors), update formErrors with new error
+const onFormChange = (name, value) => {
+
+}
+
+// formSubmit
+// add formValues state to newUser object, cleaning up datea as it goes
+// call helper function to post newUser to api (the helper updates user state)
+const formSubmit = () => {
+  const newUser = {
+    name: formValues.name.trim(),
+    email: formValues.email.trim(),
+    password: formValues.password.trim(),
+    terms: formValues.terms,
+  }
+  postNewUser(newUser);
+}
 // Side effects
 
+// disabled button - updates on formValues change. 
+// .isValid returns promise. if true, enable button
+useEffect(() => {
 
+}, [formValues])
 
+// JSX //
   return (
     <div className="App">
-      <Form />
+      <Form 
+        formValues = {formValues}
+        onFormChange = {onFormChange}
+        formSubmit = {formSubmit}
+        disabled = {disabled}
+        formErrors = {formErrors}
+      />
+      {
+        users.map()
+      }
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -3,7 +3,20 @@ import * as yup from 'yup'
 import Axios from 'axios'
 
 function Form(props){
+    const { formValues, onFormChange, formSubmit, disabled, formErrors } = props;
+
+    const onSubmit = evt => {
+        evt.preventDefault();
+        formSubmit()
+    }
+
+    const onChange = evt => {
+        const { name, value, type, checked } = evt.target
+        const valueToUse = type === 'checkbox' ? checked : value;
+        onFormChange(name, value);
+    }
+
     
-}
+} // end Form
 
 export default Form;
